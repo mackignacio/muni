66342eb4a5dfd467640779f8bddaedbfb0d65076
renzocabarios <renzo.cabarios@gmail.com>
2024-10-16 20:10:59 +0800

    Add constants

---
+++ lib/constants/index.dart
@@ -0,0 +1,19 @@
+import 'dart:ui';
+
+class Constants {
+  static const url = "http://localhost:9000/";
+
+  static const green700 = Color.fromRGBO(4, 139, 76, 1);
+
+  static const red500 = Color.fromRGBO(214, 48, 49, 1);
+  static const red400 = Color.fromRGBO(222, 89, 90, 1);
+  static const red100 = Color.fromRGBO(242, 191, 191, 1);
+
+  static const gray50 = Color.fromRGBO(234, 235, 235, 1);
+  static const gray200 = Color.fromRGBO(158, 162, 163, 1);
+  static const gray500 = Color.fromRGBO(45, 52, 54, 1);
+  static const gray600 = Color.fromRGBO(41, 47, 49, 1);
+
+  static const white = Color.fromRGBO(255, 255, 255, 1);
+  static const black = Color.fromRGBO(0, 0, 0, 1);
+}
